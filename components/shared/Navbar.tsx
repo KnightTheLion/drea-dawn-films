d6e5c0840417687e5f8b2236513fe2ebdb97d9c3
KnightTheLion <joshuaknightmitchell@gmail.com>
2024-01-02 18:36:39 -0500

    made entire navbar change dynamincally to give an easy route back home for UX/UI

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -21,9 +21,9 @@ export default function Navbar() {
       <div className="py-6">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 w-full items-center px-2 lg:px-6 justify-center">
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 w-full items-center justify-end order-2 lg:order-1 text-right lg:text-center content">
-            <div><Link href={`${pathname === '/' ? '/photos' : '/'}`}>{`${pathname === '/' ? 'PHOTOS' : 'HOME'}`}</Link></div>
-            <div><Link href={'/videos'}>VIDEOS</Link></div>
-            <div><Link href={'/kindwords'}>KIND WORDS</Link></div>
+            <div><Link href={`${pathname !== '/photos' ? '/photos' : '/'}`}>{`${pathname !== '/photos' ? 'PHOTOS' : 'HOME'}`}</Link></div>
+            <div><Link href={`${pathname !== '/videos' ? '/videos' : '/'}`}>{`${pathname !== '/videos' ? 'VIDEOS' : 'HOME'}`}</Link></div>
+            <div><Link href={`${pathname !== '/kindwords' ? '/kindwords' : '/'}`}>{`${pathname !== '/kindwords' ? 'KIND WORDS' : 'HOME'}`}</Link></div>
           </div>
           {/* MIDDLE / TOP ON MOBILE */}
           <div className="flex justify-end lg:justify-self-center order-1 lg:order-2 lg:w-fit">
@@ -41,8 +41,8 @@ export default function Navbar() {
           </div>
           {/* RIGHT SIDE / BOTTOM ON MOBILE */}
           <div className="grid grid-cols-1 lg:grid-cols-2 lg:justify-evenly gap-4 w-full items-center order-3 text-right lg:text-center">
-            <div><Link href={'/meet'}>MEET THE ARTIST</Link></div>
-            <div><Link href={'/contact'}>CONTACT</Link></div>
+            <div><Link href={`${pathname !== '/meet' ? '/meet' : '/'}`}>{`${pathname !== '/meet' ? 'MEET THE ARTIST' : 'HOME'}`}</Link></div>
+            <div><Link href={`${pathname !== '/contact' ? '/contact' : '/'}`}>{`${pathname !== '/contact' ? 'CONTACT' : 'HOME'}`}</Link></div>
           </div>
         </div>
       </div>
