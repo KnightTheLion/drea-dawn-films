bef895ab7e678df6d2e1bf4371a2e0281e98d496
KnightTheLion <joshuaknightmitchell@gmail.com>
2024-01-29 07:10:29 -0500

    reverting back to orginal way to load images

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'ik.imagekit.io',
-            port: '',
-            pathname: '/ktlwebdev/**',
-          },
-        ],
-      },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
