2e06af7b1e54fe75c53a06b1dbc8a14883e9ad33
KnightTheLion <joshuaknightmitchell@gmail.com>
2025-07-28 09:21:58 -0400

    idk, here we are

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,37 @@ export const metadata: Metadata = {
     "NC",
   ],
   metadataBase: new URL("https://dreadawn.com"),
+  openGraph: {
+    title: "Drea Dawn Photo + Films",
+    description:
+      "Drea Dawn Photography and Films is here to capture your most important memories and make them last forever. We specialize in weddings, engagements, and family photography and videography. Based in the Triad, North Carolina.",
+    url,
+    siteName: "Drea Dawn Photo + Films",
+    images: [
+      {
+        url: `${url}/opengraph-image.jpg`,
+        alt: "Drea Dawn Photo + Films — Authentic moments. Cinematic memories.",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    title: "Drea Dawn Photo + Films",
+    description:
+      "Drea Dawn Photography and Films is here to capture your most important memories and make them last forever. We specialize in weddings, engagements, and family photography and videography. Based in the Triad, North Carolina.",
+    card: "summary_large_image",
+    site: "@dreadawn",
+    creator: "@dreadawn",
+    images: [
+      {
+        url: `${url}/twitter-image.jpg`,
+        alt: "Drea Dawn Photo + Films — Authentic moments. Cinematic memories.",
+      },
+    ],
+  },
+  robots: { index: true, follow: true, nocache: true },
+  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({
