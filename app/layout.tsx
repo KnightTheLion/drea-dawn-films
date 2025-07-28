ed50e58859e697823d6870ccc0352b98adf20549
KnightTheLion <joshuaknightmitchell@gmail.com>
2025-07-28 09:06:20 -0400

    did i freaking do it yet?

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,28 @@ export const metadata: Metadata = {
     siteName: "Drea Dawn Photo + Films",
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: `${url}/opengraph-image.jpg`,
+        width: 1200,
+        height: 630,
+        type: "image/jpeg",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Drea Dawn Photo + Films",
+    description:
+      "Wedding and family photography and films in Greensboro and the Triad, North Carolina.",
+    images: [
+      {
+        url: `${url}/opengraph-image.jpg`,
+        width: 1200,
+        height: 630,
+        type: "image/jpeg",
+      },
+    ],
   },
 };
 
